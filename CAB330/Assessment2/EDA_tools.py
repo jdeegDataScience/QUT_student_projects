b5765bd22cf79283828c6881be8a3ee9ce959ffb
jdeegDataScience <139721588+jdeegDataScience@users.noreply.github.com>
2023-10-20 18:15:17 +1000

    Some handy EDA tools
    Consolidating some of the repetitive EDA things in this assessment into some functions.
    
    Will continue to add over time.

---
+++ CAB330/Assessment2/EDA_tools.py
@@ -0,0 +1,24 @@
+# check if any columns contain NA values incorrectly encoded as blank strings, i.e., ''
+# returns a list containing the names of columns of interest
+def empty_string_values_in_columns(df):
+    # empty list to record columns containing blank string values 
+    naughty_columns = []
+
+    # check each column
+    for col in range(len(df.columns)):
+        # store name of current col
+        current_col = df.columns[col]
+        
+        # records if each value in current_col is NOT a blank string
+        bools = df[current_col] != ''
+
+        # record current_col if it contains a blank string value
+        if not bools.all():
+            naughty_columns.append(current_col)
+    # there are columns of interest
+    if len(naughty_columns) > 0:
+        print("Columns to investigate:")
+        print(naughty_columns)
+    else:
+        print("No empty strings found in any columns.")
+    return naughty_columns
