da33cc6cc174ddef11a603debc88f58423350c58
jdeegDataScience <139721588+jdeegDataScience@users.noreply.github.com>
2023-10-25 14:18:36 +1000

    Code for Task 3 Questions

--- CAB330/Assessment2/task_3_TEXT_questions.py
+++ CAB330/Assessment2/task_3_TEXT_questions.py
@@ -24,56 +24,15 @@ from math import sqrt
 # M_metadata_v1.csv
 movie_data = pd.read_csv('M_metadata_v1.csv', na_filter=False)
 
-# Initialize stopwords here and more
-# initialise resources
+# initialise resources and constants
+rs = 21
 lemmatizer = WordNetLemmatizer()
 punct = set(string.punctuation)
-stopwords = set(sw.words('english')).union(set(('c', 'r', 'u', 'film')))
-stopwords = set(sw.words('english'))
-rs = 21
+# stopwords are not initialised here as different sets are used throughout the analysis
 
-def lemmatize(token, tag):
-    tag = {
-        'N': wn.NOUN,
-        'V': wn.VERB,
-        'R': wn.ADV,
-        'J': wn.ADJ
-    }.get(tag[0], wn.NOUN)
-    return lemmatizer.lemmatize(token, tag)
-
-def cab_tokenizer(document):
-    # initialize token list
-    tokens = []
-    
-    # split the document into sentences
-    for sent in sent_tokenize(document):
-        # split the document into tokens and then create part of speech tag for each token
-        for token, tag in pos_tag(wordpunct_tokenize(sent)):
-            # preprocess and remove unnecessary characters
-            token = token.lower()
-            token = token.strip()
-            token = token.strip('_')
-            token = token.strip('*')
-
-            # If stopword, ignore token and continue
-            if token in stopwords:
-                continue
-
-            # If punctuation, ignore token and continue
-            if all(char in punct for char in token):
-                continue
-
-            # Lemmatize the token and add back to the tokens list
-            lemma = lemmatize(token, tag)
-            tokens.append(lemma)
-    
-    return tokens
-
-import seaborn as sns
-import matplotlib.pyplot as plt
-
+# initialise functions for task
 # function to visualise text cluster
-def visualise_text_cluster(n_clusters, cluster_centers, terms, num_word = 5):
+def visualise_text_cluster(n_clusters, cluster_centers, terms, num_word = 10):
     # -- Params --
     # cluster_centers: cluster centers of fitted/trained KMeans/other centroid-based clustering
     # terms: terms used for clustering
@@ -84,7 +43,7 @@ def visualise_text_cluster(n_clusters, cluster_centers, terms, num_word = 5):
     
     for cluster in range(n_clusters):
         print("Top terms for cluster {}:".format(cluster), end=" ")
-        for term_idx in ordered_centroids[cluster, :5]:
+        for term_idx in ordered_centroids[cluster, :num_word]:
             print(terms[term_idx], end=', ')
         print()
 
@@ -137,25 +96,29 @@ def visualise_zipf(terms, itr_step = 50):
             prev_x, prev_y = x, y
     
     plt.show()
-  
-def preprocess_movie_data(raw_movie_data, optimise=True):
-    # drop all cols except 'Description'
-    preprocess_df = raw_movie_data.loc[:, ['Description']]
-    
-    # if default analysis
-    if optimise==True:
-        stopwords = set(sw.words('english'))
-        tfidf_vec = TfidfVectorizer(tokenizer=cab_tokenizer, ngram_range=(1,2))
-    else: # add additional stop words and strip noisy phrases from data
-        stopwords = set(sw.words('english')).union(set(('c', 'r', 'u', 'film')))
-        preprocess_df['Description'] = preprocess_df['Description'].str.split('\. --',expand=True).iloc[:,0]
-        tfidf_vec = TfidfVectorizer(tokenizer=cab_tokenizer, ngram_range=(1,2), min_df=0.05, max_df=0.7)
-    
-    X = tfidf_vec.fit_transform(preprocess_df.Description)
-    
-    return tfidf_vec, X
-  
-# Task 3.4
+
+
+# ---
+
+# 3.2
+# no stopwords required for default ZIPF plot
+stopwords = ''
+# slow, run infrequently
+terms = calculate_tf_idf_terms(movie_data.Description)
+visualise_zipf(terms)
+
+# ---
+
+# 3.3
+# additional terms identified during analysis that are...
+# not useful for clustering and...
+# that are not contained in the preset stop words list
+movie_stop = set(('c', 'r', 'u', 'film'))
+print(movie_stop)
+
+# ---
+
+# 3.4
 # tf idf vectoriser
 tfidf_vec = TfidfVectorizer(tokenizer=cab_tokenizer, ngram_range=(1,2))
 X = tfidf_vec.fit_transform(movie_data.Description)
@@ -163,75 +126,58 @@ X = tfidf_vec.fit_transform(movie_data.Description)
 # number of input features
 print(len(tfidf_vec.get_feature_names_out()))
 
-# Task 3.6 ?
-# K means clustering using the term vector
-kmeans = KMeans(n_clusters=10, random_state=rs).fit(X)
+# ---
 
-# call it
-visualise_text_cluster(kmeans.n_clusters, kmeans.cluster_centers_, tfidf_vec.get_feature_names_out())
-
-# Task 3.2
-terms = calculate_tf_idf_terms(movie_data.Description)
-visualise_zipf(terms)
-
-# Task 3.4 - trying another tf idf vectoriser, to compare
-# tf idf vectoriser
-filter_vec = TfidfVectorizer(tokenizer=cab_tokenizer, ngram_range=(1,2),
-                            min_df=2, max_df=0.8)
-X_filter = filter_vec.fit_transform(movie_data.Description)
-
-# number of input features
-print(len(filter_vec.get_feature_names_out()))
-
-# K means clustering using the new term vector, time it for comparison to SVD
-kmeans_fil = KMeans(n_clusters=10, random_state=rs).fit(X_filter)
-
-# visualisation
-visualise_text_cluster(kmeans_fil.n_clusters, kmeans_fil.cluster_centers_, 
-                       filter_vec.get_feature_names_out())
-
-# K-means clustering - SVD
-svd = TruncatedSVD(n_components=100, random_state=42)
+# 3.5 Optimal K
+# apply SVD/LSA transformation 
+svd = TruncatedSVD(n_components=100, random_state=rs)
 X_trans = svd.fit_transform(X_filter)
 
-# sort the components by largest weighted word
-sorted_comp = svd.components_.argsort()[:, ::-1]
-terms = filter_vec.get_feature_names_out()
+# list to save the clusters and cost
+clusters = []
+inertia_vals = []
+explore_range = range(17, 22, 1)
 
-# visualise word - concept/component relationships
-for comp_num in range(10):
-    print("Top terms in component #{}".format(comp_num), end=" ")
-    for i in sorted_comp[comp_num, :5]:
-        print(terms[i], end=", ")
-    print()
+for k in explore_range:
+    # train clustering with the specified K
+    model = KMeans(n_clusters=k, random_state=rs)
+    model.fit(X_trans)
+    
+    # append model to cluster list
+    clusters.append(model)
+    inertia_vals.append(model.inertia_)
+
+# plot the inertia vs K values
+plt.plot(explore_range, inertia_vals, marker='*')
+plt.show()
+
+# programmatically generate and store silhouette scores for later
+cluster_silhouettes = {}
+
+for clust in range(len(clusters)):
+    key = clusters[clust].n_clusters
+    value = silhouette_score(X_trans, clusters[clust].predict(X_trans))
+    cluster_silhouettes[key] = value
+    
+#    print(clusters[clust])
+#    print(f"Silhouette score for k={key}", value)
+#    print()
+
+max_sil_score = max(cluster_silhouettes.values())
+max_sil_clusters = get_key_by_value(cluster_silhouettes, max_sil_score)
+print("Number of clusters: ", max_sil_score, "\nSilhouette Score: ", max_sil_clusters)
+
+# ---
+
+# 3.6 - Describe Clusters and Display results
+# number of clusters
+print("Number of clusters: ", max_sil_clusters, "\nSilhouette Score: ", max_sil_score)
 
 # K-means clustering using LSA-transformed X
-svd_kmeans = KMeans(n_clusters=10, random_state=rs).fit(X_trans)
+svd_kmeans = KMeans(n_clusters=max_sil_clusters, random_state=rs).fit(X_trans)
 
 # transform cluster centers back to original feature space for visualisation
 original_space_centroids = svd.inverse_transform(svd_kmeans.cluster_centers_)
 
 # visualisation
-visualise_text_cluster(svd_kmeans.n_clusters, original_space_centroids, filter_vec.get_feature_names_out())
-
-# Task 3.5 - finding optimal K - using the elbow method
-#list to save the clusters 
-clusters = []
-inertia_vals = []
-
-# this whole process should take a while
-for k in range(2, 15, 2):
-    # train clustering with the specified K
-    model = KMeans(n_clusters=k, random_state=rs)
-    model.fit(X)
-    
-    # append model to cluster list
-    clusters.append(model)
-    inertia_vals.append(model.inertia_)
-    
-# plot the inertia vs K values
-plt.plot(range(2,15,2), inertia_vals, marker='*')
-plt.xlabel('Number of Clusters (k)')
-plt.ylabel('Inertia')
-plt.title('Elbow Method for Optimal k')
-plt.show()
+visualise_text_cluster(svd_kmeans.n_clusters, original_space_centroids, tfidf_filter.get_feature_names_out(), num_word = 15)
