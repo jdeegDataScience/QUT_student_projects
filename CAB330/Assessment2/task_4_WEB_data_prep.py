402be1ec802feee65b3a0efdf43d1afd3f9cd2fe
jdeegDataScience <139721588+jdeegDataScience@users.noreply.github.com>
2023-10-26 14:03:06 +1000

    Redesign Data Prep for Linear Reg
    All captured username's are assigned to 1 column for conciseness.
    
    Only page URLs (ending in '.php') from 'requests' column are captured. Only the page name (exc. '.php') captured.
    
    'sessionLength' calculation from web_session_enrichment function refactored into main body, executed before grouping.
    
    'page' one-hot encoding executed after 'sessionLength' calculated and before grouping on session.
    
    'preprocess_web_data' function only returns columns from 'sessionLength' onwards (8th column, index 7) as these are all that is required for linear regression.

--- CAB330/Assessment2/task_4_WEB_data_prep.py
+++ CAB330/Assessment2/task_4_WEB_data_prep.py
@@ -18,33 +18,39 @@ def preprocess_web_data(raw_web_data):
     # extract usernames
     usernames = preprocess_df['request'].str.extractall('.+nm=([\w]+)|\?user=([\w]+)|\?name=([\w]+)|show=([\w]+)')
     usernames = usernames.fillna('')
-    usernames['username'] = usernames[0] + usernames[1] + usernames[2]
-    usernames['UN_shown'] = usernames[3]
-    usernames = usernames[['username', 'UN_shown']].droplevel(level=1)
+    usernames['username'] = usernames[0] + usernames[1] + usernames[2] + usernames[3]
+    usernames = usernames['username'].droplevel(level=1)
     preprocess_df = preprocess_df.merge(usernames, left_index=True, right_index=True, how='left')
     
     # convert empty values back to NAs
-    preprocess_df['UN_shown'] = preprocess_df['UN_shown'].replace('', np.nan).astype(object)
     preprocess_df['username'] = preprocess_df['username'].replace('', np.nan).astype(object)
     
-    # parse method from requests
-    #preprocess_df['method'] = preprocess_df['request'].str.split(' ',expand=True)[0]
+    # parse pages from requests
     request_splits = preprocess_df['request'].str.split(expand=True)
-    preprocess_df['method'] = request_splits[0]
-    preprocess_df['request'] = request_splits[1]
-    preprocess_df['protocol'] = request_splits[2]
-    # Add 'conversion' column
-    preprocess_df['conversion'] = preprocess_df['request'].str.contains('contestsubmission', case=False)
-
-    # reset index
-    preprocess_df.reset_index(drop=True, inplace=True)
+    preprocess_df['page'] = request_splits[1].str.extract('/(.*)\.php')
+    
+    # drop request column
+    preprocess_df.drop(columns='request', inplace=True)
 
     # apply function above to get a new dataframe with added information
     web_sessions = preprocess_df.apply(get_log_user_info, axis=1)
 
-    web_sessions_enriched = session_clustering_enrichment(web_sessions)
+    # Drop rows where 'page' is NaN
+    web_sessions = web_sessions.dropna(subset=['page'])
     
-    return web_sessions_enriched #web_sessions
+    # Add 'sessionLength' column
+    web_sessions['sessionLength'] = web_sessions.groupby('session')['time'].apply(lambda x: (x.max() - x.min()).seconds)
+    # Drop rows where 'sessionLength' is NaN
+    web_sessions.dropna(subset=['sessionLength'], inplace=True)
+    # Filter and drop rows where 'sessionLength' is less than 1 (Bot traffic)
+    web_sessions = web_sessions[web_sessions['sessionLength'] >= 1]
+    
+    web_sessions = pd.get_dummies(web_sessions, columns=['page'], prefix=[''], prefix_sep=[''])
+    
+    web_sessions = web_sessions.groupby('session').last().reset_index()
+    
+    # return only the required columns
+    return web_sessions.iloc[:,7:]
 
 
 
@@ -84,43 +90,3 @@ def get_log_user_info(row):
     row['userID'] = user_id_dict[user_key]
     session_steps[row['session']] += 1
     return row
-
-
-
-def session_clustering_enrichment(web_sessions):
-    # Create session_request_counts DataFrame
-    session_request_counts = web_sessions.groupby('session')['request'].nunique().reset_index()
-    session_request_counts.rename(columns={'request': 'requests_per_session'}, inplace=True)
-    
-    # Add the 'method' column to session_request_counts
-    session_request_counts['method'] = web_sessions.groupby('session')['method'].last().values
-    
-    # Assuming session_request_counts has columns: ['sessionLength', 'requests_per_session', 'isLoggedIn', 'UserID', 'IP Address']
-    columns_to_plot = ['sessionLength', 'requests_per_session', 'isLoggedIn', 'returnUser', 'conversion']
-    
-    # Add the 'Username' column to session_request_counts
-    session_request_counts['username'] = web_sessions.groupby('session')['username'].last().values
-    # Fill NaN values in the 'Username' column with a default value
-    session_request_counts['username'].fillna('Guest', inplace=True)
-    # Create a binary variable indicating whether the user was isLoggedIn
-    session_request_counts['isLoggedIn'] = session_request_counts['username'] != 'Guest'
-    
-    # Add 'IP Address' column
-    session_request_counts['ip'] = web_sessions.groupby('session')['ip'].last().values
-    # Identify returnUser based on matching IP addresses
-    session_request_counts['returnUser'] = session_request_counts.duplicated(subset='ip')
-
-    # Add 'sessionLength' column
-    session_request_counts['sessionLength'] = web_sessions.groupby('session')['time'].apply(lambda x: (x.max() - x.min()).seconds)
-    # Drop rows where 'sessionLength' is NaN
-    session_request_counts = session_request_counts.dropna(subset=['sessionLength'])
-    # Filter and drop rows where 'sessionLength' is less than 1 (Bot traffic)
-    session_request_counts = session_request_counts[session_request_counts['sessionLength'] >= 1]
-
-    # Add 'conversion' column
-    session_request_counts['conversion'] = web_sessions.groupby('session')['conversion'].max().astype(bool)
-    
-    # Extract features for clustering
-    features_for_clustering = session_request_counts[columns_to_plot]
-
-    return features_for_clustering
