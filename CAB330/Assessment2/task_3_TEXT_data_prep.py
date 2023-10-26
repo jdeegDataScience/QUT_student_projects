184ea6d976b38a04b21afb7c9b8f409c0bf88232
jdeegDataScience <139721588+jdeegDataScience@users.noreply.github.com>
2023-10-26 15:15:24 +1000

    Refactor and narrow down term filtering params

--- CAB330/Assessment2/task_3_TEXT_data_prep.py
+++ CAB330/Assessment2/task_3_TEXT_data_prep.py
@@ -40,13 +40,11 @@ def preprocess_movie_data(raw_movie_data, optimise=False):
     preprocess_df = raw_movie_data.loc[:, ['Description']]
     
     # if default analysis
-    if optimise==False:
-        stopwords = set(sw.words('english'))
+    if optimise==False: # default vectorizer with unigram and bigram tokens
         tfidf_vec = TfidfVectorizer(tokenizer=cab_tokenizer, ngram_range=(1,2))
-    else: # add additional stop words and strip noisy phrases from data
-        stopwords = set(sw.words('english')).union(set(('c', 'r', 'u', 'film')))
+    else: # strip noisy phrases from data and filter terms by frequency
         preprocess_df['Description'] = preprocess_df['Description'].str.split('\. --',expand=True).iloc[:,0]
-        tfidf_vec = TfidfVectorizer(tokenizer=cab_tokenizer, ngram_range=(1,2), min_df=0.05, max_df=0.7)
+        tfidf_vec = TfidfVectorizer(tokenizer=cab_tokenizer, ngram_range=(1,2), min_df=0.05, max_df=0.3)
     
     X = tfidf_vec.fit_transform(preprocess_df.Description)
     
