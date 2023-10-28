950c11924718eb36d7287ad86ed9ee940d462eb8
jdeegDataScience <139721588+jdeegDataScience@users.noreply.github.com>
2023-10-28 11:13:30 +1000

    Substantial Changes
    Added an alternate version of the 'cab_tokenizer' function ('cab_tokenizer_no_stop') because it is called directly in 'calculate_tf_idf_terms' function. When called from this function it circumvented the conditional parameter in the 'preprocess_movie_data' function, resulting in all stop words always being stripped which is not the intended effect at 3.2.
    
    'cab_tokenizer' function adjusted to contain the stopwords in the definition. This was in an attempt to trouble-shoot the term 'film' being included in the analysis despite being in the stopwords list.
    Later, I realised this is because the stopwords are filtered out BEFORE the tokens are lemmatised, meaning that alternate forms of the word (e.g. 'films', 'filming') would be would not be filtered out and their lemma ('film') would then be included as a toke. 
    
    The 'English' stopwords are stored as a separate variable to the identified corpus-specific stopwords, each token is checked against each set (and the punctuation set) to generate a boolean variables; if any of the variables are true, the token is neither lemmatised nor vectorised.
    
    
    In the 'preprocess_movie_data' function, the 'default' processing now includes a print statement to alert the user that stopwords are not being filtered out and, implicitly, terms are not being filtered by document frequency.

--- CAB330/Assessment2/task_3_TEXT_data_prep.py
+++ CAB330/Assessment2/task_3_TEXT_data_prep.py
@@ -7,7 +7,51 @@ def lemmatize(token, tag):
     }.get(tag[0], wn.NOUN)
     return lemmatizer.lemmatize(token, tag)
 
-def cab_tokenizer(document):
+def cab_tokenizer(document):    
+    # initialize token list
+    tokens = []
+    
+    # explicit stopword variables
+    engStop = sw.words('english')
+    
+    movieStop = set(('c', 'r', 'u', 'film', 'films',
+                     'life', 'love', 'one', 'story', 'lives', 'living',
+                     'director', 'direct', 'directs', 'directing',
+                     'feature', 'features', 'featuring',
+                     'star', 'stars', 'starring',
+                     'world', 'rovi', 'award', 'academy',
+                     'year', 'years', 'two', 
+                     'find', 'finds', 'power', 'powers', 'powerful',
+                     'include', 'includes', 'including',
+                     'also', 'first', 'filmmaker', 'movie', 'movies',
+                     'dream', 'would', 'use'))
+    
+    # split the document into sentences
+    for sent in sent_tokenize(document):
+        # split the document into tokens and then create part of speech tag for each token
+        for token, tag in pos_tag(wordpunct_tokenize(sent)):
+            # preprocess and remove unnecessary characters
+            token = token.lower()
+            token = token.strip()
+            token = token.strip('_')
+            token = token.strip('*')
+            
+            isEngStop = token in engStop
+            
+            isMovieStop = token in movieStop
+            
+            isPunct = all(char in punct for char in token)
+            
+
+            # If not stopword or punctuation
+            if not isEngStop and not isMovieStop and not isPunct:
+                # Lemmatize the token and add back to the tokens list
+                lemma = lemmatize(token, tag)
+                tokens.append(lemma)
+
+    return tokens
+
+def cab_tokenizer_no_stop(document):    
     # initialize token list
     tokens = []
     
@@ -20,32 +64,35 @@ def cab_tokenizer(document):
             token = token.strip()
             token = token.strip('_')
             token = token.strip('*')
+            
+            # if token is just punctuation
+            isPunct = all(char in punct for char in token)
+            
 
-            # If stopword, ignore token and continue
-            if token in stopwords:
-                continue
+            # If not punctuation
+            if not isPunct:
+                # Lemmatize the token and add back to the tokens list
+                lemma = lemmatize(token, tag)
+                tokens.append(lemma)
 
-            # If punctuation, ignore token and continue
-            if all(char in punct for char in token):
-                continue
-
-            # Lemmatize the token and add back to the tokens list
-            lemma = lemmatize(token, tag)
-            tokens.append(lemma)
-    
     return tokens
 
 def preprocess_movie_data(raw_movie_data, optimise=False):
     # drop all cols except 'Description'
     preprocess_df = raw_movie_data.loc[:, ['Description']]
     
-    # if default analysis
-    if optimise==False: # default vectorizer with unigram and bigram tokens
-        tfidf_vec = TfidfVectorizer(tokenizer=cab_tokenizer, ngram_range=(1,2))
-    else: # strip noisy phrases from data and filter terms by frequency
-        preprocess_df['Description'] = preprocess_df['Description'].str.split('\. --',expand=True).iloc[:,0]
-        tfidf_vec = TfidfVectorizer(tokenizer=cab_tokenizer, ngram_range=(1,2), min_df=0.065, max_df=0.3)
+    if optimise: # strip noisy phrases from data and filter terms
+        preprocess_df['Description'] = preprocess_df['Description'].str.split(r'\. --',expand=True).iloc[:,0]
+        tfidf_vec = TfidfVectorizer(tokenizer=cab_tokenizer, ngram_range=(1,2), min_df=0.06, max_df=0.3)
     
+    # otherwise, default analysis
+    else: # default vectorizer with unigram and bigram tokens
+        tfidf_vec = TfidfVectorizer(tokenizer=cab_tokenizer_no_stop, ngram_range=(1,2))
+        print("Filter stopwords: ", optimise)
+    
+    X = tfidf_vec.fit_transform(preprocess_df.Description)
+    
+    return tfidf_vec, X
     X = tfidf_vec.fit_transform(preprocess_df.Description)
     
     return tfidf_vec, X
