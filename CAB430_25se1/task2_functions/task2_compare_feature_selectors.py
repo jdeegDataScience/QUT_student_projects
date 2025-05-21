6150aeacdce09435f6d97d92e6ab04c5fd9bfd11
jdeegDataScience <139721588+jdeegDataScience@users.noreply.github.com>
2025-05-21 13:19:35 +1000

    Create task2_compare_feature_selectors.py

---
+++ CAB430_25se1/task2_functions/task2_compare_feature_selectors.py
@@ -0,0 +1,59 @@
+# import pandas as pd
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+
+# from sklearn.feature_selection import SelectKBest, f_classif, mutual_info_classif
+# from sklearn.model_selection import train_test_split
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.naive_bayes import GaussianNB
+# from sklearn.metrics import accuracy_score, classification_report
+
+def compare_feat_selectors(X, y, num_features, feat_select_score_funcs,
+                           classifier_names=["Decision Tree Classifier", "Gaussian Naive Bayes"], rs=42):
+    
+    # vars to store feature selection results
+    top_sel_feats = pd.DataFrame(dtype='S')
+    scores_df = pd.DataFrame(index=X.columns.sort_values())
+
+    # loop score functions
+    for i in range(len(feat_select_score_funcs)):
+        np.random.seed(rs)  # where not explicitly supplied, reset rng seed before each use
+        # fit selector
+        selector = SelectKBest(score_func=feat_select_score_funcs[i][1], k=num_features).fit(X, y)
+
+        # extract + store scores, feature names
+        func_scores = pd.Series(selector.scores_, index=selector.feature_names_in_, name="attribute").sort_index()
+        scores_df[feat_select_score_funcs[i][0]] = func_scores
+        top_sel_feats[feat_select_score_funcs[i][0]] = func_scores.nlargest(n=num_features, keep='all').index.to_list()
+    
+    # df to store model fit results
+    model_accuracies = pd.DataFrame(columns=['accuracy', 'dataset', 'classifier'])
+    
+    # loop features:classifiers
+    for col in top_sel_feats.columns:
+        # Re‐slice X to selected features of curr scoring func 
+        X_sel = X[top_sel_feats[col].tolist()]
+
+        # Split out 20% for model testing 
+        X_train, X_test, y_train, y_test = train_test_split(
+            X_sel, y, test_size=0.2, random_state=rs)
+
+        # Train the decision tree
+        dt = DecisionTreeClassifier(random_state=rs)
+        dt.fit(X_train, y_train)
+        
+        dt_accuracy = pd.DataFrame({'accuracy':[dt.score(X_train, y_train), dt.score(X_test, y_test)],
+         'dataset':["training", "test"], 'classifier':[classifier_names[0], classifier_names[0]]})
+        model_accuracies = pd.concat([model_accuracies, dt_accuracy], ignore_index=True)
+
+        # Train the GNB
+        gnb = GaussianNB()
+        gnb.fit(X_train, y_train)
+        gnb_accuracy = pd.DataFrame({'accuracy':[gnb.score(X_train, y_train), gnb.score(X_test, y_test)],
+         'dataset':["training", "test"], 'classifier':[classifier_names[1], classifier_names[1]]})
+        model_accuracies = pd.concat([model_accuracies, gnb_accuracy], ignore_index=True)
+    
+    scores_df.reset_index(inplace=True)
+    scores_df.rename({'index':'Attribute'}, axis=1, inplace=True)
+    
+    return top_sel_feats, scores_df, model_accuracies
