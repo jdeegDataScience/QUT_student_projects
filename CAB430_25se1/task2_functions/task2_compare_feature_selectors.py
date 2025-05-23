795cecc9e28d65b9fdf6394a327fbda5a3154a43
jdeegDataScience <139721588+jdeegDataScience@users.noreply.github.com>
2025-05-23 15:05:59 +1000

    rm "classifier" from DT classifier name to improve readability

--- CAB430_25se1/task2_functions/task2_compare_feature_selectors.py
+++ CAB430_25se1/task2_functions/task2_compare_feature_selectors.py
@@ -9,7 +9,7 @@
 # from sklearn.metrics import accuracy_score, classification_report
 
 def compare_feat_selectors(X, y, num_features, feat_select_score_funcs, packages_arr,
-                           classifier_names=["Decision Tree Classifier", "Gaussian Naive Bayes"], rs=42):
+                           classifier_names=["Decision Tree", "Gaussian Naive Bayes"], rs=42):
   # packages_arr = [pd, np, SelectKBest, train_test_split, DecisionTreeClassifier, GaussianNB]
   pd = packages_arr[0]
   np = packages_arr[1]
